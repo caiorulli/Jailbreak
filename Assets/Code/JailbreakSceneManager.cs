30f6f74c0fd2085249d4cb14d0a687b6ffc8b7f6
Caio Rulli <caiorulli@gmail.com>
2018-04-25 22:23:32 -0300

    Started game flow

---
+++ Assets/Code/JailbreakSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JailbreakSceneManager : MonoBehaviour {
+
+    [SerializeField]
+    private bool _autoLoad;
+
+    [SerializeField]
+    private string _initialScene;
+
+    private static JailbreakSceneManager _instance;
+    public static JailbreakSceneManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("SceneManager not in scene");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        if (_autoLoad)
+        {
+            Invoke("LoadScene", 5);
+        }
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(_initialScene);
+    }
+}
