a56c3bc3d9089f75f0caf8357b53500815b59969
gmarcon17 <guilhermemarcon12@gmail.com>
2018-05-02 19:28:13 -0300

    Add flow changes

--- Assets/Code/PlayerDetection.cs
+++ Assets/Code/PlayerDetection.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerDetection : MonoBehaviour
-{
+{   
+    JailbreakSceneManager _JailbreakSceneManager = new JailbreakSceneManager();
+    // public JailbreakSceneManager _JailbreakSceneManager;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
-            UnityEditor.EditorApplication.isPlaying = false;
+            _JailbreakSceneManager.LoadScene("Game Over");
+            // UnityEditor.EditorApplication.isPlaying = false;
         }
     }
 }
