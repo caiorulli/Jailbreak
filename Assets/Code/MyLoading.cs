928906065c80a31096bc42376a9dc1d397f72cd3
gmarcon17 <guilhermemarcon12@gmail.com>
2018-05-02 21:41:39 -0300

    probuild

---
+++ Assets/Code/MyLoading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyLoading : MonoBehaviour {
+	static string nextScene;
+
+	public static string NextScene
+	{
+		get 
+		{
+			return nextScene;
+		}
+
+		set
+		{
+			nextScene = value;
+		}
+	}
+
+
+	void Start () {
+		SceneManager.LoadScene(NextScene);
+	}
+	
+	void Update () {
+		
+	}
+}
