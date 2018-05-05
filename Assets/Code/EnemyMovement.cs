cb6476011d155ddf952f7155418beb1264fc1dbb
gmarcon17 <guilhermemarcon12@gmail.com>
2018-05-05 15:52:55 -0300

    rota customizada

--- Assets/Code/EnemyMovement.cs
+++ Assets/Code/EnemyMovement.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
+
+// Montei uma rota ponto por ponto para mostrar para o professor,
+// depois vou fazer algo mais inteligente
 
 public class EnemyMovement : MonoBehaviour {
     [SerializeField]
@@ -8,18 +12,22 @@ public class EnemyMovement : MonoBehaviour {
     private NavMeshAgent _agent;
     private Transform[] _waypoints;
     private int _currentWaypoint;
+    private int _previousWaypoint;
+
+    private int _auxValue;
 
 	void Start () {
         _agent = GetComponent<NavMeshAgent>();
         _waypoints = _route.GetComponentsInChildren<Transform>();
-
-        _currentWaypoint = Random.Range(1, _waypoints.Length) - 1;
+        _currentWaypoint = 1;
+        _previousWaypoint = 0;
+        // _currentWaypoint = Random.Range(1, _waypoints.Length) - 1;
         _agent.SetDestination(GetNextWaypointPosition());
 	}
 	
 	void Update () {
         Vector3 distanceToNextWaypoint = _waypoints[_currentWaypoint].position - transform.position;
-        if (distanceToNextWaypoint.magnitude < 3)
+        if (distanceToNextWaypoint.magnitude < 2.5)
         {
             SwitchToNextWaypoint();
             _agent.SetDestination(GetNextWaypointPosition());
@@ -33,6 +41,40 @@ public class EnemyMovement : MonoBehaviour {
 
     private void SwitchToNextWaypoint()
     {
-        _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Length;
+        _auxValue = _previousWaypoint;
+        _previousWaypoint = _currentWaypoint;
+
+        if (_currentWaypoint == 1) {
+            if (_auxValue == 0 || _auxValue == 5) {
+                _currentWaypoint = 2;
+            }
+            else if (_auxValue == 2) {
+                _currentWaypoint = 4;
+            }
+            else if (_auxValue == 4) {
+                _currentWaypoint = 5;
+            }
+        }
+
+        else if (_currentWaypoint == 2){
+            if (_auxValue == 1) {
+                _currentWaypoint = 3;
+            }
+            else if (_auxValue == 3) {
+                _currentWaypoint = 1;
+            }
+        }
+
+        else if (_auxValue == 3) {
+            _currentWaypoint = 2;
+        }
+
+        else if (_auxValue == 4 || _auxValue == 5) {
+            _currentWaypoint = 1;
+        }
+
+        // else {
+        //  _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Length;
+        // }
     }
 }
