84738be4bb6132b339d2d1aaca65dba5c97c30a6
Caio <caiorulli@gmail.com>
2018-05-22 23:50:21 -0300

    Added lantern and soundtrack

--- Assets/Code/EnemyMovement.cs
+++ Assets/Code/EnemyMovement.cs
@@ -3,10 +3,9 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
     [SerializeField]
-    private GameObject _route;
+    private GameObject[] _waypoints;
 
     private NavMeshAgent _agent;
-    private Transform[] _waypoints;
     private int _currentWaypoint;
     private int _previousWaypoint;
 
@@ -14,15 +13,12 @@ public class EnemyMovement : MonoBehaviour {
 
 	void Start () {
         _agent = GetComponent<NavMeshAgent>();
-        _waypoints = _route.GetComponentsInChildren<Transform>();
-        _currentWaypoint = 1;
-        _previousWaypoint = 0;
-        // _currentWaypoint = Random.Range(1, _waypoints.Length) - 1;
+        _currentWaypoint = 0;
         _agent.SetDestination(GetNextWaypointPosition());
 	}
 	
 	void Update () {
-        Vector3 distanceToNextWaypoint = _waypoints[_currentWaypoint].position - transform.position;
+        Vector3 distanceToNextWaypoint = _waypoints[_currentWaypoint].transform.position - transform.position;
         if (distanceToNextWaypoint.magnitude < 2.5)
         {
             SwitchToNextWaypoint();
@@ -32,45 +28,11 @@ public class EnemyMovement : MonoBehaviour {
 
     private Vector3 GetNextWaypointPosition()
     {
-        return _waypoints[_currentWaypoint].position;
+        return _waypoints[_currentWaypoint].transform.position;
     }
 
     private void SwitchToNextWaypoint()
     {
-        _auxValue = _previousWaypoint;
-        _previousWaypoint = _currentWaypoint;
-
-        if (_currentWaypoint == 1) {
-            if (_auxValue == 0 || _auxValue == 5) {
-                _currentWaypoint = 2;
-            }
-            else if (_auxValue == 2) {
-                _currentWaypoint = 4;
-            }
-            else if (_auxValue == 4) {
-                _currentWaypoint = 5;
-            }
-        }
-
-        else if (_currentWaypoint == 2){
-            if (_auxValue == 1) {
-                _currentWaypoint = 3;
-            }
-            else if (_auxValue == 3) {
-                _currentWaypoint = 1;
-            }
-        }
-
-        else if (_auxValue == 3) {
-            _currentWaypoint = 2;
-        }
-
-        else if (_auxValue == 4 || _auxValue == 5) {
-            _currentWaypoint = 1;
-        }
-
-        // else {
-        //  _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Length;
-        // }
+        _currentWaypoint = (_currentWaypoint + 1) % _waypoints.Length;
     }
 }
